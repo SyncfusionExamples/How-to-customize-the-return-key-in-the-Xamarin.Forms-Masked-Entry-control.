784cd79564f73a99a817989d14e6d899396b7f0a
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2021-05-11 17:36:01 +0530

    XAMARIN-37419- How to customize the return key in the Xamarin.Forms Masked Entry control sample committed

---
+++ ReturnKey_MaskedEdit/ReturnKey_MaskedEdit/Updater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ReturnKey_MaskedEdit
+{
+    class Updater : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ ReturnKey_MaskedEdit/ReturnKey_MaskedEdit/ViewModel/ViewModel .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ReturnKey_MaskedEdit
+{
+    public class ViewModel
+    {
+        private ICommand commandUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (commandUpdater == null)
+                    commandUpdater = new Updater();
+                return commandUpdater;
+            }
+            set
+            {
+                commandUpdater = value;
+            }
+        }
+    }
+
+  
+}
